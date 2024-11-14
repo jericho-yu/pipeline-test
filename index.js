8206897248bd509f44b56c670ab396619614914d
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2024-11-14 12:00:35 +0800

    m

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ import fs from 'fs/promises';
 
 
 async function main() {
+	let xmlStart = `<?xml version='1.1' encoding='UTF-8'?>
+<flow-definition plugin="workflow-job@1385.vb_58b_86ea_fff1">
+  <actions>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@2.2151.ve32c9d209a_3f"/>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@2.2151.ve32c9d209a_3f">
+      <jobProperties/>
+      <triggers/>
+      <parameters/>
+      <options/>
+    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
+  </actions>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <jenkins.model.BuildDiscarderProperty>
+      <strategy class="hudson.tasks.LogRotator">
+        <daysToKeep>7</daysToKeep>
+        <numToKeep>5</numToKeep>
+        <artifactDaysToKeep>-1</artifactDaysToKeep>
+        <artifactNumToKeep>-1</artifactNumToKeep>
+      </strategy>
+    </jenkins.model.BuildDiscarderProperty>
+  </properties>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@3826.v3b_5707fe44da_">
+    <script>`;
+	let xmlEnd = `</script>
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
+</flow-definition>`;
+
 	// 读取文件
 	try {
 		const fileContentJson = await fs.readFile('./config.json', 'utf8');
@@ -13,10 +45,10 @@ async function main() {
 		let content = generatePipeline(fileContent, level);
 
 		// 保存文本到文件
-		await fs.writeFile('./pipeline', content, 'utf8');
-		console.log('Pipeline script saved to pipeline.txt');
+		await fs.writeFile('./pipeline.xml', `${xmlStart}${content}${xmlEnd}`, 'utf8');
+		console.log('Pipeline script saved to pipeline.xml');
 	} catch (err) {
-		console.error("Error reading file"+err)
+		console.error("Error reading file" + err)
 		return;
 	}
 }
